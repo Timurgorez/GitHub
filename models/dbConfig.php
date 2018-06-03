458ecccd67ffa58cf1c6aa36999c0bb312a9d927
Timurgorez <timuumit@mail.ru>
2018-06-03 23:11:50 +0300

    Add files via upload

---
+++ models/dbConfig.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class DBConfig {
+
+    public static $defConfig = [
+            'host' => 'localhost',
+            'port' => '3306',
+            'db_name' => 'shop',
+            'admin' => 'root',
+            'pass' => '',
+        ];
+
+}
